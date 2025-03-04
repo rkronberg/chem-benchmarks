442b06f7b2267c3a4c7ea245b15d5bddf495c60d
Rasmus Kronberg <43936697+rkronberg@users.noreply.github.com>
2025-03-04 13:13:30 +0200

    Update mahti-mpi128-omp2.sh

--- slurm/mahti-mpi128-omp2.sh
+++ slurm/mahti-mpi128-omp2.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=project_2001659
 #SBATCH --partition=test
 #SBATCH --nodes=2
-#SBATCH --ntasks-per-node=128
+#SBATCH --ntasks-per-node=64
 #SBATCH --cpus-per-task=2
 #SBATCH --time=0-1
 #SBATCH --output=logs/slurm-%x-%j.out
