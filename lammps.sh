f02dc1d221bf03ea7ac30c67f063e87f76f37836
Rasmus Kronberg <rasmus.kronberg@csc.fi>
2026-02-20 10:01:17 +0200

    add lmp

--- lammps.sh
+++ lammps.sh
@@ -1,8 +1,13 @@
 SCRIPT="lmp"
-ARGS="-in benchmarks/in.lj -log logs/log-$SLURM_JOBID.lammps -k on t ${OMP_NUM_THREADS} g ${SLURM_GPUS_PER_NODE} -sf kk -pk kokkos -v x 8 -v y 8 -v z 8"
+ARGS="-in $1 -log logs/log-$SLURM_JOBID.lammps -v x 4 -v y 4 -v z 4"
 
-export OMP_PROC_BIND=spread
-export OMP_PLACES=threads
+if [[ $SLURM_GPUS_PER_NODE -gt 0 ]]; then
+    ARGS="$ARGS -k on t ${OMP_NUM_THREADS} g ${SLURM_GPUS_PER_NODE} -sf kk -pk kokkos"
+    export OMP_PROC_BIND=spread
+    export OMP_PLACES=threads
+else
+    ARGS="$ARGS -sf omp"
+fi
 
 (set -x
 srun $SRUN_OPTS $SCRIPT $ARGS
---
+++ slurm/lumi-mpi2048-omp1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=project_462000007
+#SBATCH --partition=standard
+#SBATCH --nodes=16
+#SBATCH --ntasks-per-node=128
+#SBATCH --cpus-per-task=1
+#SBATCH --time=30
+#SBATCH --output=logs/slurm-%x-%j.out
+
+export OMP_NUM_THREADS=1
+
+cd $SLURM_SUBMIT_DIR
+
+source slurm/common.sh
