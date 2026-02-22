75fba37e26f8f368b48ad8776a6e21e587081e0e
Rasmus Kronberg <rasmus.kronberg@csc.fi>
2026-02-22 18:06:10 +0200

    update

--- amber.sh
+++ amber.sh
@@ -1,14 +1,26 @@
-ARGS="-O -i benchmarks/mdin.GPU -p benchmarks/cellulose.prmtop -c benchmarks/cellulose.inpcrd -o logs/mdout.GPU -x logs/mdcrd -inf logs/mdinfo -r logs/restrt -l logs/logfile"
-
-if [[ $HOSTNAME == "nid"* ]]; then
-    SCRIPT="pmemd.hip"
+if [[ $SLURM_GPUS_PER_NODE -gt 0 ]]; then
+    if [[ $HOSTNAME == "nid"* ]]; then
+        SCRIPT="pmemd.hip"
+        source slurm/lumi-affinity.sh
+    else
+        SCRIPT="pmemd.cuda"
+    fi
 else
-    SCRIPT="pmemd.cuda"
+    SCRIPT="pmemd.MPI"
 fi
 
-if [[ $SLURM_GPUS_ON_NODE -gt 1 ]]; then
+if [[ $SLURM_GPUS_PER_NODE -gt 1 ]]; then
     SCRIPT="$SCRIPT.MPI"
 fi
+
+ARGS="-O -i $1.mdin.GPU \
+      -p $1.prmtop \
+      -c $1.inpcrd \
+      -o logs/mdout-$SLURM_JOBID \
+      -x logs/mdcrd-$SLURM_JOBID \
+      -inf logs/mdinfo-$SLURM_JOBID \
+      -r logs/restrt-$SLURM_JOBID \
+      -l logs/logfile-$SLURM_JOBID"
 
 (set -x
 srun $SRUN_OPTS $SCRIPT $ARGS
---
+++ slurm/lumi-mpi2-omp1-gcd2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=project_462000007
+#SBATCH --partition=standard-g
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --gpus-per-node=2
+#SBATCH --time=15
+#SBATCH --output=logs/slurm-%x-%j.out
+
+export OMP_NUM_THREADS=1
+export MPICH_GPU_SUPPORT_ENABLED=1
+
+cd $SLURM_SUBMIT_DIR
+
+source slurm/common.sh
