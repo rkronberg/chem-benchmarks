62a430e5f3e14b52006a84a7a73d9a50ec595f72
Rasmus Kronberg <rakronbe@csc.fi>
2026-03-26 09:56:47 +0200

    fix script

--- slurm/roihu-mpi192-omp2.sh
+++ slurm/roihu-mpi192-omp2.sh
@@ -2,11 +2,12 @@
 #SBATCH --account=project_2001659
 #SBATCH --partition=test
 #SBATCH --nodes=1
-#SBATCH --ntasks=384
+#SBATCH --ntasks=192
+#SBATCH --cpus-per-task=2
 #SBATCH --time=15
 #SBATCH --output=logs/slurm-%x-%j.out
 
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=2
 
 cd $SLURM_SUBMIT_DIR
 source slurm/common.sh
