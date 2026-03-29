559a75d46e2db2d4486c5f223af5f02b58781e62
Rasmus Kronberg <rakronbe@csc.fi>
2026-03-29 10:58:07 +0300

    less output from cp2k

---
+++ slurm/roihu-mpi96-omp4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --account=project_2001659
+#SBATCH --partition=medium
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=96
+#SBATCH --cpus-per-task=4
+#SBATCH --time=15
+#SBATCH --output=logs/slurm-%x-%j.out
+#SBATCH --hint=nomultithread
+
+export OMP_NUM_THREADS=4
+
+cd $SLURM_SUBMIT_DIR
+source slurm/common.sh
