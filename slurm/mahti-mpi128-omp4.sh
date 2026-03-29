0c4e5f7da7967b53391c86a4be0d3ea2cdb58df4
Rasmus Kronberg <rasmus.kronberg@csc.fi>
2026-03-29 21:04:58 +0300

    mahti

---
+++ slurm/mahti-mpi128-omp4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --account=project_2001659
+#SBATCH --partition=medium
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=4
+#SBATCH --time=0-1
+#SBATCH --output=logs/slurm-%x-%j.out
+
+export OMP_NUM_THREADS=4
+
+cd $SLURM_SUBMIT_DIR
+source slurm/common.sh
