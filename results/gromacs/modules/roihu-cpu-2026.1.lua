3cc194c1d10f6423123049e181550f16ed197743
Rasmus Kronberg <rakronbe@csc.fi>
2026-04-06 10:48:40 +0300

    modules

---
+++ results/gromacs/modules/roihu-cpu-2026.1.lua
@@ -0,0 +1,10 @@
+activeList = {
+  StdEnv = "",
+  fftw = "3.3.10",
+  gcc = "15.2.0",
+  gromacs = "2026.1",
+  openblas = "0.3.30",
+  openmpi = "5.0.8",
+  ucx = "1.19.0",
+}
+
---
+++ results/gromacs/modules/roihu-gpu-2025.4.lua
@@ -0,0 +1,10 @@
+activeList = {
+  StdEnv = "",
+  cuda = "12.9.1",
+  gcc = "14.3.0",
+  ["git-lfs"] = "3.7.0",
+  gromacs = "2025.4",
+  openblas = "0.3.30",
+  openmpi = "5.0.8",
+}
+
