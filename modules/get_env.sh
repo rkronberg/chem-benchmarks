f3dc69cd0e540d8c76c92bdb4aa78fdf067d35f0
Rasmus Kronberg <rasmus.kronberg@csc.fi>
2026-04-06 14:23:59 +0300

    move modules

--- modules/get_env.sh
+++ modules/get_env.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 MODULE="$1"
-SYSTEM="$2"
+HOST="$2"
 
-if [[ -z "$MODULE" || -z "$SYSTEM" ]]; then
-    echo "Usage: $0 <module_name> <system>"
+if [[ -z "$MODULE" || -z "$HOST" ]]; then
+    echo "Usage: $0 <module_name> <host>"
     exit 1
 fi
 
-MODULE_NAME="${MODULE%%/*}"
-MODULE_VERSION="${MODULE##*/}"
-OUTPUT_FILE="${MODULE_NAME}/modules/${SYSTEM}-${MODULE_VERSION}.lua"
+OUTPUT_FILE="modules/${MODULE}-${HOST}.lua"
 
 if module load "$MODULE" 2>/dev/null; then
     echo "Writing environment to: $OUTPUT_FILE"
